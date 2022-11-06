dfa6ee61bf1f2145b3192db241380b796d8be417
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-11-06 10:43:38 +0100

    added workoutOfTheDay.js

---
+++ models/workoutOfTheDay.js
@@ -0,0 +1,53 @@
+import mongoose, { Schema } from 'mongoose';
+
+const DailyWorkoutSchema = new mongoose.Schema({
+  workoutName: {
+    type: String,
+    maxlength: 50,
+  },
+  ex1: {
+    type: String,
+    maxlength: 50,
+  },
+  ex2: {
+    type: String,
+    maxlength: 50,
+  },
+  ex3: {
+    type: String,
+    maxlength: 50,
+  },
+  ex4: {
+    type: String,
+    maxlength: 50,
+  },
+  ex5: {
+    type: String,
+    maxlength: 50,
+  },
+  ex6: {
+    type: String,
+    maxlength: 50,
+  },
+  ex7: {
+    type: String,
+    maxlength: 50,
+  },
+  ex8: {
+    type: String,
+    maxlength: 50,
+  },
+  difficulty: {
+    type: String,
+    maxlength: 50,
+  },
+  link: {
+    type: String,
+    maxlength: 120,
+  },
+  duration: {
+    type: Number,
+  },
+});
+
+export default mongoose.model('DailyWorkout', DailyWorkoutSchema);
