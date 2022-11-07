33be382e0698e9a350c5aa65098e5a176a1c49a9
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-11-07 20:02:22 +0100

    cleaned the code, added comments

--- public/js/workoutTimer.js
+++ public/js/workoutTimer.js
@@ -2,26 +2,15 @@ const counter = document.querySelector('.counter');
 const startButton = document.querySelector('.btnStart');
 const upButton = document.querySelector('.btnUp');
 const downButton = document.querySelector('.btnDown');
-//new
-// const textOne = document.getElementById('lunch');
-// const btn1 = document.getElementById('btnTest');
 
 startButton.addEventListener('click', timerInt);
 upButton.addEventListener('click', add);
 downButton.addEventListener('click', sub);
-// btn1.addEventListener('click', fun1);
 
 let userTime = 45;
 let initialTime = 45;
 counter.innerHTML = initialTime;
 let interval;
-
-// //new
-// function fun1() {
-//   initialTime = textOne.value;
-//   userTime = textOne.value;
-//   counter.innerHTML = initialTime;
-// }
 
 function timerInt() {
   startButton.classList.add('disable');
