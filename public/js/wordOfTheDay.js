b65b9de6219900e80c0ce0bd7ebb387a860fc845
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-11-08 18:09:08 +0100

    removed What to study option from word of the day

--- public/js/wordOfTheDay.js
+++ public/js/wordOfTheDay.js
@@ -1,14 +1,6 @@
 const button = document.querySelector('.start-btn');
 const buttonTwo = document.querySelector('.start-btn-two');
 
-const subjectData = [
-  { subject: 'Read Book' },
-  { subject: 'Write One Page' },
-  { subject: 'Listen PodCast' },
-  { subject: 'Watch Serie' },
-  { subject: 'Generate random word and write 5 sentences' },
-];
-
 let deuWord = '';
 let engWord = '';
 let randomSubject = '';
@@ -24,8 +16,8 @@ const getAllWords = async () => {
       wordsApiFetch.push(word);
       return;
     });
-  } catch (error) {
-    console.log(error);
+  } catch (e) {
+    console.log(e);
   }
 };
 
@@ -38,35 +30,13 @@ const randomWordGenerator = () => {
 
   engWord = wordsApiFetch[random].word;
 
+  console.log(wordsApiFetch[random]._id);
+
   document.getElementById('germanWord').innerHTML = deuWord;
 
   document.getElementById('englishWord').innerHTML = engWord;
 };
 
-const randomSubjectGenerator = () => {
-  const random = Math.floor(Math.random() * subjectData.length);
-
-  randomSubject = subjectData[random].subject;
-
-  document.getElementById('whatToStudy').innerHTML = randomSubject;
-};
-
 button.addEventListener('click', () => {
   randomWordGenerator();
 });
-
-buttonTwo.addEventListener('click', () => {
-  randomSubjectGenerator();
-});
-/* setInterval(randomWordGenerator, 6000); */
-
-/* function timer() {
-  setInterval();
-}
-
-const clearAlert = () => {
-  setTimeout(() => {
-    dispatch({ type: CLEAR_ALERT });
-  }, 3000);
-};
- */
