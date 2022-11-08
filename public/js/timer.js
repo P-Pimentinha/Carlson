f919c60f35e3223f035aa31fd55999c4158b82f8
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-11-08 22:20:00 +0100

    added minutes to timer

--- public/js/timer.js
+++ public/js/timer.js
@@ -48,16 +48,21 @@
 
 // New Timer
 const countdown = document.getElementById('countdown');
-const startButton = document.querySelector('.btnStartEl');
-const upButton = document.querySelector('.btnUpEl');
-const downButton = document.querySelector('.btnDownEL');
+const startButton = document.querySelector('.btnStart');
+const upButton = document.querySelector('.btnUp');
+const downButton = document.querySelector('.btnDown');
 
-let startingMinutes = 1;
-let startingSeconds = 5;
+let UserStartingMinutes = 0;
+let UserStartingSeconds = 45;
+
+let startingMinutes = 0;
+let startingSeconds = 45;
 countdown.innerHTML = `${startingMinutes}:${startingSeconds}`;
 let interval;
 
 startButton.addEventListener('click', initiateTimer);
+upButton.addEventListener('click', add);
+downButton.addEventListener('click', sub);
 
 function initiateTimer() {
   startButton.classList.add('disable');
@@ -71,17 +76,48 @@ function timer() {
     startButton.classList.remove('disable');
     startButton.removeAttribute('disabled', '');
     clearInterval(interval);
-    startingMinutes = 1;
-    startingSeconds = 5;
+    startingMinutes = UserStartingMinutes;
+    startingSeconds = UserStartingSeconds;
     countdown.innerHTML = `${startingMinutes}:${startingSeconds}`;
     return;
-  } else if ((startingSeconds === 0) & (startingMinutes != 0)) {
-    startingMinutes -= 1;
-    startingSeconds = 4;
-  } else {
-    console.log('hello');
   }
+
+  if ((startingSeconds === 0) & (startingMinutes != 0)) {
+    startingMinutes -= 1;
+    startingSeconds = 60;
+  }
+
   startingSeconds--;
   // prettier-ignore
+  return (countdown.innerHTML = `${startingMinutes}:${startingSeconds}`);
+}
+
+function add() {
+  if (startingSeconds >= 59) {
+    startingSeconds = 0;
+    UserStartingSeconds = 0;
+    startingMinutes += 1;
+    UserStartingMinutes += 1;
+    return (countdown.innerHTML = `${startingMinutes}:${startingSeconds}`);
+  }
+
+  startingSeconds += 1;
+  UserStartingSeconds += 1;
+  return (countdown.innerHTML = `${startingMinutes}:${startingSeconds}`);
+}
+
+function sub() {
+  if ((startingMinutes === 0) & (startingSeconds === 0)) return;
+
+  if (startingSeconds === 0) {
+    startingMinutes -= 1;
+    UserStartingMinutes -= 1;
+    startingSeconds = 59;
+    UserStartingSeconds = 59;
+    return (countdown.innerHTML = `${startingMinutes}:${startingSeconds}`);
+  }
+
+  startingSeconds -= 1;
+  UserStartingSeconds -= 1;
   return (countdown.innerHTML = `${startingMinutes}:${startingSeconds}`);
 }
