6d6cfc3ae61473d98a613fafafaf69f142bb7abe
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-11-08 20:47:39 +0100

    created function timer

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,16 +1,63 @@
-const counter = document.querySelector('.counter');
-const startButton = document.querySelector('.btnStart');
-const upButton = document.querySelector('.btnUp');
-const downButton = document.querySelector('.btnDown');
+// const counter = document.querySelector('.counter');
+// const startButton = document.querySelector('.btnStart');
+// const upButton = document.querySelector('.btnUp');
+// const downButton = document.querySelector('.btnDown');
+
+// startButton.addEventListener('click', initiateTimer);
+// upButton.addEventListener('click', add);
+// downButton.addEventListener('click', sub);
+
+// let userTime = 45;
+// let initialTime = 45;
+// counter.innerHTML = initialTime;
+// let interval;
+
+// function initiateTimer() {
+//   startButton.classList.add('disable');
+//   startButton.setAttribute('disabled', '');
+//   interval = setInterval(timer, 1000);
+// }
+
+// function timer() {
+//   if (initialTime === 0) {
+//     // startButton.removeEventListener('click', timerInt);
+//     startButton.classList.remove('disable');
+//     startButton.removeAttribute('disabled', '');
+//     clearInterval(interval);
+//     initialTime = userTime;
+//     counter.innerHTML = initialTime;
+//     return;
+//   }
+//   initialTime--;
+//   // prettier-ignore
+//   return counter.innerHTML = initialTime;
+// }
+
+// function add() {
+//   initialTime += 1;
+//   userTime += 1;
+//   counter.innerHTML = initialTime;
+// }
+
+// function sub() {
+//   if (initialTime === 0) return;
+//   initialTime -= 1;
+//   userTime -= 1;
+//   counter.innerHTML = initialTime;
+// }
+
+// New Timer
+const countdown = document.getElementById('countdown');
+const startButton = document.querySelector('.btnStartEl');
+const upButton = document.querySelector('.btnUpEl');
+const downButton = document.querySelector('.btnDownEL');
+
+let startingMinutes = 1;
+let startingSeconds = 5;
+countdown.innerHTML = `${startingMinutes}:${startingSeconds}`;
+let interval;
 
 startButton.addEventListener('click', initiateTimer);
-upButton.addEventListener('click', add);
-downButton.addEventListener('click', sub);
-
-let userTime = 45;
-let initialTime = 45;
-counter.innerHTML = initialTime;
-let interval;
 
 function initiateTimer() {
   startButton.classList.add('disable');
@@ -19,39 +66,22 @@ function initiateTimer() {
 }
 
 function timer() {
-  if (initialTime === 0) {
+  if ((startingMinutes === 0) & (startingSeconds === 0)) {
     // startButton.removeEventListener('click', timerInt);
     startButton.classList.remove('disable');
     startButton.removeAttribute('disabled', '');
     clearInterval(interval);
-    initialTime = userTime;
-    counter.innerHTML = initialTime;
+    startingMinutes = 1;
+    startingSeconds = 5;
+    countdown.innerHTML = `${startingMinutes}:${startingSeconds}`;
     return;
+  } else if ((startingSeconds === 0) & (startingMinutes != 0)) {
+    startingMinutes -= 1;
+    startingSeconds = 4;
+  } else {
+    console.log('hello');
   }
-  initialTime--;
+  startingSeconds--;
   // prettier-ignore
-  return counter.innerHTML = initialTime;
+  return (countdown.innerHTML = `${startingMinutes}:${startingSeconds}`);
 }
-
-function add() {
-  initialTime += 1;
-  userTime += 1;
-  counter.innerHTML = initialTime;
-}
-
-function sub() {
-  if (initialTime === 0) return;
-  initialTime -= 1;
-  userTime -= 1;
-  counter.innerHTML = initialTime;
-}
-
-// New Timer
-const constdownEl = document.getElementById('countdown');
-const startButtonEl = document.querySelector('.btnStartEL');
-const upButtonEl = document.querySelector('.btnUpEl');
-const downButtonEl = document.querySelector('.btnDownEL');
-
-let startingMinutes = 0;
-let startingSeconds = 45;
-constdownEl.innerHTML = `${startingMinutes}:${startingSeconds}`;
