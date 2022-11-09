d32fd7112cffb445a351e4e464bacabeee6d724d
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2022-11-09 18:54:02 +0100

    chore: delete getWorkout route/controller

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -4,11 +4,9 @@ const router = express.Router();
 import {
   createWorkout,
   getWorkouts,
-  getWorkout,
 } from '../controllers/workoutController.js';
 
 router.route('/').get(getWorkouts).post(createWorkout);
-router.route('/id?').get(getWorkout);
 
 /* router.route('/').get(getWords); */
 
